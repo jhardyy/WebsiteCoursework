0307560e3276be0f904b50bf38e73d440fa3bb3e
p15197905 <p15197905@my365.dmu.ac.uk>
2017-12-05 14:43:11 +0000

    Started to add code for a pop up login screen

---
+++ JS/Welcome.js
@@ -0,0 +1,13 @@
+﻿var today = new Date();
+var TheCurrentHour = today.getHours();
+var Greeting;
+if (TheCurrentHour > 18) {
+    Greeting = 'Good evening, welcome to The DMU Restaurant.';
+} else if (TheCurrentHour > 12) {
+    Greeting = 'Good afternoon, welcome to The DMU Restaurant.';
+} else if (TheCurrentHour > 0) {
+    Greeting = 'Good Morning, welcome to The DMU Restaurant.';
+} else {
+    Greeting = 'Welcome to The DMU Restaurant.';
+}
+document.write(Greeting);
