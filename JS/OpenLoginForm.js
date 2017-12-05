65461bec44853b0607586ebeaf9fa24a3aba6332
jhardyy <p15197905@my365.dmu.ac.uk>
2017-12-05 20:49:48 +0000

    Swapped onclick over to an eventlistener for login button

--- JS/OpenLoginForm.js
+++ JS/OpenLoginForm.js
@@ -1,12 +1,8 @@
 ﻿// Get the modal
-var LoginButton = document.getElementById('LoginPopUp')
+var LoginButton = document.getElementById('Login').addEventListener('click', OpenLoginForm);
+var LoginForm = document.getElementById('LoginPopUp');
 
-
-window.addEventListener('click', CloseWindow);
-
-function CloseWindow(event) {
-    if (event.target == LoginForm || event.target == CloseX) {
-        LoginForm.style.display = "none";
-    }
+function OpenLoginForm() {
+        LoginForm.style.display = "block";
 }
 
