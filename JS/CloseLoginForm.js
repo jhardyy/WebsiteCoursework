2c57dcbc8ac27f19a43adae5a4dc4f120147f397
jhardyy <p15197905@my365.dmu.ac.uk>
2017-12-05 20:43:28 +0000

    added OpenLoginForm.js for the button that opens the login form.

---
+++ JS/CloseLoginForm.js
@@ -0,0 +1,13 @@
+﻿// Get the modal
+var LoginForm = document.getElementById('LoginPopUp')
+var CloseX = document.getElementById('CloseButton')
+
+window.addEventListener('click', CloseWindow);
+
+function CloseWindow(event) {
+    if (event.target == LoginForm || event.target == CloseX) {
+        LoginForm.style.display = "none";
+    }
+}
+
+
---
+++ JS/OpenLoginForm.js
@@ -0,0 +1,12 @@
+﻿// Get the modal
+var LoginButton = document.getElementById('LoginPopUp')
+
+
+window.addEventListener('click', CloseWindow);
+
+function CloseWindow(event) {
+    if (event.target == LoginForm || event.target == CloseX) {
+        LoginForm.style.display = "none";
+    }
+}
+
