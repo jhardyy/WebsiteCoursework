80c3a5e0d6f8c3e94a21d32f70629c696ca94500
jhardyy <p15197905@my365.dmu.ac.uk>
2017-12-29 18:08:42 +0000

    More changes to try and fix gitpages

--- JS/OpenLoginForm.js
+++ JS/OpenLoginForm.js
@@ -2,13 +2,13 @@
 //Add an eventlistener to listen for a click on to the element with the ID 'LoginBtn'.
 //If there is a click, execute the function 'OpenLoginForm'
 let LoginButton = document.getElementById('LoginBtn').addEventListener('click', OpenLoginForm);
-//create a variable called 'loginForm' and assign it to the element with the ID 'LoginPopUp'.
-let LoginForm = document.getElementById('LoginPopUp');
+//create a variable called 'loginFormpopup' and assign it to the element with the ID 'LoginPopUp'.
+let LoginFormPopup = document.getElementById('LoginPopUp');
 
 
 //create a function called OpenLoginForm.
 function OpenLoginForm() {
         //Make the LoginForm appear to the user by setting its display to block.
-        LoginForm.style.display = "block";
+        LoginFormPopup.style.display = "block";
 }
 
